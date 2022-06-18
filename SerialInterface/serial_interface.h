8252dde5cf4c3eb0b30ab88d0ebca3116de86188
usama <mahmoodify@gmail.com>
2022-06-18 16:54:51 +0500

    Adding settings prop

--- SerialInterface/serial_interface.h
+++ SerialInterface/serial_interface.h
@@ -1,11 +1,15 @@
 #ifndef SERIAL_INTERFACE_H
 #define SERIAL_INTERFACE_H
 
+#include "settings.h"
 
 class SerialInterface
 {
 public:
     SerialInterface();
+
+private:
+    static Settings _settings;
 };
 
 #endif // SERIAL_INTERFACE_H
