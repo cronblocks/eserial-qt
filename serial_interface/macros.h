59df2db51c29522d174269fb6dfbbd6e38b8b648
usama <mahmoodify@gmail.com>
2022-06-18 21:08:14 +0500

    Defining debug msg and msg line macros

--- serial_interface/macros.h
+++ serial_interface/macros.h
@@ -1,4 +1,12 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+#if _DEBUGGING_MESSAGES == 1
+    #define DEBUG_MSG(msg)      {std::cout << msg;}
+    #define DEBUG_MSG_LINE(msg) {std::cout << msg << std::endl;}
+#else
+    #define DEBUG_MSG(msg)
+    #define DEBUG_MSG_LINE(msg)
+#endif
+
 #endif // MACROS_H
