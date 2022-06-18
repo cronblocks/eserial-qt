75c01c33ff49ff08fb926f2439f793f6aa3d6246
usama <mahmoodify@gmail.com>
2022-06-18 16:52:29 +0500

    Renaming class files

--- main_window.h
+++ main_window.h
@@ -1,5 +1,5 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
 
 #include <QMainWindow>
 
@@ -18,4 +18,4 @@ public:
 private:
     Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
+#endif // MAIN_WINDOW_H
