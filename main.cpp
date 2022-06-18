4a8e50f021b0e9109e2078a9bbc7124db8c06cd1
usama <mahmoodify@gmail.com>
2022-06-18 17:30:03 +0500

    Updating generated ui file refs

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication application(argc, argv);
+
+    MainWindow main_window;
+    main_window.show();
+
+    return application.exec();
 }
