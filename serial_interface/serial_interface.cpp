e755ec713e29e560e34b48cee09a490c4f7bd3b6
usama <mahmoodify@gmail.com>
2022-06-19 16:24:48 +0500

    Adding interline delay in settings storage

--- serial_interface/serial_interface.cpp
+++ serial_interface/serial_interface.cpp
@@ -3,11 +3,12 @@
 #include "macros.h"
 
 
-#define SETTING_KEY_BAUD_RATE       "BaudRate"
-#define SETTING_KEY_DATA_BITS       "DataBits"
-#define SETTING_KEY_PARITY          "Parity"
-#define SETTING_KEY_STOP_BITS       "StopBits"
-#define SETTING_KEY_LINE_ENDING     "LineEnding"
+#define SETTING_KEY_BAUD_RATE           "BaudRate"
+#define SETTING_KEY_DATA_BITS           "DataBits"
+#define SETTING_KEY_PARITY              "Parity"
+#define SETTING_KEY_STOP_BITS           "StopBits"
+#define SETTING_KEY_LINE_ENDING         "LineEnding"
+#define SETTING_KEY_INTERLINE_DELAY     "InterlineDelay"
 
 
 SerialInterface::SerialInterface() {
@@ -35,6 +36,10 @@ SerialInterface::SerialInterface() {
                 m_settings_ptr->getIntValue(
                     SETTING_KEY_LINE_ENDING, static_cast<int>(LineEnding::LF)));
 
+    m_interline_delay_ms = static_cast<unsigned int>(
+                m_settings_ptr->getUIntValue(
+                    SETTING_KEY_INTERLINE_DELAY, 50));
+
     // --------------
     // Connectors
     // ------------
@@ -58,6 +63,8 @@ void SerialInterface::stopInterface() {
     m_settings_ptr->setIntValue( SETTING_KEY_PARITY,      static_cast<int>( m_parity      ));
     m_settings_ptr->setIntValue( SETTING_KEY_STOP_BITS,   static_cast<int>( m_stop_bits   ));
     m_settings_ptr->setIntValue( SETTING_KEY_LINE_ENDING, static_cast<int>( m_line_ending ));
+
+    m_settings_ptr->setUIntValue(SETTING_KEY_INTERLINE_DELAY, m_interline_delay_ms);
 
     m_discvr_ptr->stopDiscoveringPorts();
     m_settings_ptr->saveSettings();
