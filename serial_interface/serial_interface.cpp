6720e51d60a3c98c3e09781e7b8a2631911189d0
usama <mahmoodify@gmail.com>
2022-06-19 14:57:59 +0500

    Renaming setting strings to setting keys

--- serial_interface/serial_interface.cpp
+++ serial_interface/serial_interface.cpp
@@ -3,11 +3,11 @@
 #include "macros.h"
 
 
-#define SETTING_STRING_BAUD_RATE       "BaudRate"
-#define SETTING_STRING_DATA_BITS       "DataBits"
-#define SETTING_STRING_PARITY          "Parity"
-#define SETTING_STRING_STOP_BITS       "StopBits"
-#define SETTING_STRING_LINE_ENDING     "LineEnding"
+#define SETTING_KEY_BAUD_RATE       "BaudRate"
+#define SETTING_KEY_DATA_BITS       "DataBits"
+#define SETTING_KEY_PARITY          "Parity"
+#define SETTING_KEY_STOP_BITS       "StopBits"
+#define SETTING_KEY_LINE_ENDING     "LineEnding"
 
 
 SerialInterface::SerialInterface() {
@@ -17,23 +17,23 @@ SerialInterface::SerialInterface() {
 
     m_baud_rate = static_cast<BaudRate>(
                 m_settings_ptr->getIntValue(
-                    SETTING_STRING_BAUD_RATE, static_cast<int>(BaudRate::_115200)));
+                    SETTING_KEY_BAUD_RATE, static_cast<int>(BaudRate::_115200)));
 
     m_data_bits = static_cast<DataBits>(
                 m_settings_ptr->getIntValue(
-                    SETTING_STRING_DATA_BITS, static_cast<int>(DataBits::_8)));
+                    SETTING_KEY_DATA_BITS, static_cast<int>(DataBits::_8)));
 
     m_parity = static_cast<Parity>(
                 m_settings_ptr->getIntValue(
-                    SETTING_STRING_PARITY, static_cast<int>(Parity::None)));
+                    SETTING_KEY_PARITY, static_cast<int>(Parity::None)));
 
     m_stop_bits = static_cast<StopBits>(
                 m_settings_ptr->getIntValue(
-                    SETTING_STRING_STOP_BITS, static_cast<int>(StopBits::One)));
+                    SETTING_KEY_STOP_BITS, static_cast<int>(StopBits::One)));
 
     m_line_ending = static_cast<LineEnding>(
                 m_settings_ptr->getIntValue(
-                    SETTING_STRING_LINE_ENDING, static_cast<int>(LineEnding::LF)));
+                    SETTING_KEY_LINE_ENDING, static_cast<int>(LineEnding::LF)));
 
     // --------------
     // Connectors
