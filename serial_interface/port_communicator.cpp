691c717af94de90f95803026ff60f8b58dd6cd00
usama <mahmoodify@gmail.com>
2022-06-20 14:22:27 +0500

    Setting up serial port values

--- serial_interface/port_communicator.cpp
+++ serial_interface/port_communicator.cpp
@@ -15,6 +15,24 @@ PortCommunicator::PortCommunicator(
     m_parity      = parity;
     m_stop_bits   = stop_bits;
 
+    m_serial = new QSerialPort();
+
+    m_serial->setPortName(m_serial_port);
+    m_serial->setBaudRate(static_cast<int>(m_baud_rate));
+    m_serial->setDataBits(static_cast<QSerialPort::DataBits>(m_data_bits));
+
+    switch (m_parity) {
+        case Parity::None: m_serial->setParity(QSerialPort::NoParity);   break;
+        case Parity::Even: m_serial->setParity(QSerialPort::EvenParity); break;
+        case Parity::Odd:  m_serial->setParity(QSerialPort::OddParity);  break;
+    }
+
+    switch (m_stop_bits) {
+        case StopBits::One:       m_serial->setStopBits(QSerialPort::OneStop);        break;
+        case StopBits::OnePtFive: m_serial->setStopBits(QSerialPort::OneAndHalfStop); break;
+        case StopBits::Two:       m_serial->setStopBits(QSerialPort::TwoStop);        break;
+    }
+
     m_is_running = false;
 }
 
